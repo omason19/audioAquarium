d023fe0750e5c0a0ca96b56694528e2bbefe4441
omason19 <129993922+omason19@users.noreply.github.com>
2023-05-29 18:18:55 -0700

    click areas

--- site-prototype/js/site.js
+++ site-prototype/js/site.js
@@ -1,3 +1,45 @@
+$(".box").draggable({
+    containment: ".aquarium",
+    
+});
+
+$("#tank").droppable({
+    accept: function(draggable) {
+      var groupId = draggable.parent().attr("id");
+      return $("#tank").find(".box[data-group='" + groupId + "']").length === 0;
+    },
+    drop: function(event, ui) {
+      // Move the dropped object into the container
+      ui.draggable.appendTo($(this));
+    }
+  });
+
+
+//   $(function() {
+//     // Make the objects draggable
+//     $(".box").draggable({
+//       revert: function(valid) {
+//         if (!valid) {
+//           // Reset the position of the object being dragged
+//           $(this).data("ui-draggable").originalPosition = {
+//             top: 0,
+//             left: 30
+//           };
+//         }
+//         return !valid; // Only revert if dropped outside a valid container
+//       },
+//       containment: ".aquarium" // Limit dragging within the document body
+//     });
+  
+//     // Make the container accept only one object from each group
+//     $("#fishtank").droppable({
+//       accept: function(box) {
+//         var groupId = box.parent().attr("id");
+//         return $("#container").find(".box[data-group='" + groupId + "']").length === 0;
+//       }
+//     });
+//   });
+
 // $(function() {
 //     // Make the objects draggable
 //     $(".box").draggable({
@@ -20,7 +62,7 @@
 //     });
   
 //     // Make the container accept only one object from each group
-//     $(".tank").droppable({
+//     $(".fishtank").droppable({
 //       accept: function(box) {
 //         var groupId = box.data("group");
 //         var existingObject = $(this).find(".box[data-group='" + groupId + "']");
@@ -34,8 +76,3 @@
 //       }
 //     });
 //   });
-
-$(function(){
-
-    
-})
